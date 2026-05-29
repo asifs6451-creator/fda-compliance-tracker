e3acb8884eb5a415836f47a47bc0b37a2e045ad9
asifs6451-creator <asifs6451@gmail.com>
2026-05-29 17:12:53 +0530

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -2,4 +2,4 @@
 //  STEP 1: Paste your Google Apps Script Web App URL below
 //  (Deploy > New deployment > Web App, then copy the URL)
 // ─────────────────────────────────────────────────────────────
-window.GAS_URL = 'PASTE_YOUR_GAS_URL_HERE';
+window.GAS_URL = 'https://script.google.com/a/macros/mosaicwellness.in/s/AKfycbw37RwViKuH1wxyIf6rP_I6qhMU12LOMNlJU1ZeQSrnSi4ARyA5HPL00DKDNSTMXt-X/exec';
